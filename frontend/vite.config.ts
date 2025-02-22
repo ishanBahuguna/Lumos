808b34ea720f94a2522a9df87589257190da310f
Ishan Bahuguna <ishanbahuguna1@gmail.com>
2025-02-22 21:16:51 +0530

    Optimized FE and BE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-import withMT from "@material-tailwind/react/utils/withMT";
-
 
 export default defineConfig({
   plugins: [
